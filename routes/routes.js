33bab3d86c690631071b404dd74ae532a8f03319
Maria Jose Molina <marymolina1301@gmail.com>
2024-05-04 11:15:43 +0200

    get methods

--- routes/routes.js
+++ routes/routes.js
@@ -12,8 +12,24 @@ const router = express.Router();
 
 //get all restaurants
 router.get('/restaurants', async (req, res) => {
-    const restaurant = await restaurants.find();
+    const response = await restaurants.ModelRestaurant.find();
     res.json(response); 
+}
+);
+
+//get restaurant by id
+router.get('/restaurants/:id', async (req, res) => {
+    const response = await restaurants.ModelRestaurant.findById(req.params.id);
+    res.json(response);
+}
+);
+
+//get restaurant by tags
+
+router.get('/restaurants/tags/?tags', async (req, res) => 
+{
+    const response = await restaurants.ModelRestaurant.find({tags: req.params.tags});
+    res.json(response);
 }
 );
 
