9cb52496cb82a8cddfbd9b5f8beb347ed73c56ef
Maria Jose Molina <marymolina1301@gmail.com>
2024-05-04 12:32:25 +0200

    ready for deploy5

--- routes/routes.js
+++ routes/routes.js
@@ -12,42 +12,63 @@ const router = express.Router();
 
 //get all restaurants
 router.get('/restaurants', async (req, res) => {
-    const response = await restaurants.ModelRestaurant.find();
-    res.json(response); 
+    try{
+        const response = await restaurants.ModelRestaurant.find();
+        res.json(response); 
+    }
+    catch(err){
+        console.log(err);
+    }   
 }
 );
 
 //get restaurant by id
 router.get('/restaurants/:id', async (req, res) => {
-    const response = await restaurants.ModelRestaurant.findById(req.params.id);
-    res.json(response);
+    try{
+        const response = await restaurants.ModelRestaurant.findById(req.params.id);
+        res.json(response); 
+    }
+    catch(err){
+        console.log(err);
+    }
 }
 );
 
 //get restaurant by tags
 
-router.get('/restaurants/tags/:tags', async (req, res) => 
-{
-
-
-    const response = await restaurants.ModelRestaurant.find({ tags: { $all: [ req.params.tags] }});
-    res.json(response);
+router.get('/restaurants/tags/:tags', async (req, res) => {
+    try{
+        const response = await restaurants.ModelRestaurant.find({ tags: { $all: [ req.params.tags] }});
+        res.json(response); 
+    }
+    catch(err){
+        console.log(err);
+    }
 }
 );
 
 //get restaurant by tags by id
-router.get('/restaurants/tags/:tags/:id', async (req, res) => 
-{
-    const response = await restaurants.ModelRestaurant.find({ tags: { $all: [ req.params.tags] }, _id: req.params.id });
-    res.json(response);
+router.get('/restaurants/tags/:tags/:id', async (req, res) => {
+    try{
+        const response = await restaurants.ModelRestaurant.find({ tags: { $all: [ req.params.tags] }, _id: req.params.id });
+        res.json(response); 
+    }
+    catch(err){
+        console.log(err);
+    }
 }
 );
 
 //get restaurant by city
-router.get('/restaurants/city/:city', async (req, res) => 
-{
-    const response = await restaurants.ModelRestaurant.find({ 'location.city': req.params.city });
-    res.json(response);
+router.get('/restaurants/city/:city', async (req, res) => {
+    try{
+        const response = await restaurants.ModelRestaurant.find({ 'location.city': req.params.city });
+        res.json(response); 
+    }
+    catch(err){
+        console.log(err);
+    }
+    
 }
 );
 
